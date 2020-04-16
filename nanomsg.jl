d5625ff194888c59b53f78c4a91b1bcd502393bb
Shan Sikdar <ssikdar1@bu.edu>
2020-04-16 17:50:46 -0400

    change clean up code

--- nanomsg.jl
+++ nanomsg.jl
@@ -11,8 +11,13 @@ function nn_strerror(errno::Int32)
 end
 nn_strerror(errno::Int64) = nn_strerror(Int32(errno))
 
-function nn_errno()
-    return ccall((:nn_errno, "libnanomsg"), Int32, ())
+nn_errno() = ccall((:nn_errno, "libnanomsg"), Int32, ())
+
+function check_err(func_name, rv)
+    if rv < 0
+        err = nn_strerror(nn_errno())
+        error(func_name, err)
+    end
 end
 
 """
@@ -26,12 +31,10 @@ Create an SP socket of type `protocol`.
 function nn_socket(protocol; raw=false)
     domain = !raw ? 1 : 2 # AF_SP=1 AF_SP_RAW = 2
     fd = ccall((:nn_socket, "libnanomsg"), Int32, (Int32, Int32), domain, protocol)
-    if fd < 0
-        err = nn_strerror(nn_errno())
-        error("nn_socket: ", err)
-    end
     return fd
 end
+
+
 
 """
     nn_bind(s, addr)
@@ -40,26 +43,15 @@ Bind local endpoint to socket s.
 
 Returns positive endpoint ID, else -1 for failure.
 """
-function nn_bind(s, addr)
-    rv = ccall((:nn_bind, "libnanomsg"), Int32, (Int32, Ptr{UInt8}), s, addr)
-    if rv < 0
-        err = nn_strerror(nn_errno())
-        error("nn_bind: ", err)
-    end
-    return s
-end
+nn_bind(s, addr) = ccall((:nn_bind, "libnanomsg"), Int32, (Int32, Ptr{UInt8}), s, addr)
 
 """
 Unlike with traditional BSD sockets, this function operates asynchronously,
 and returns to the caller before the operation is complete.
+
+Returns positive endpoint ID, else -1 for failure.
 """
-function nn_connect(sock, addr)
-    rv = ccall((:nn_connect, "libnanomsg"), Int32, (Int32, Ptr{UInt8}), sock, addr)
-    if rv < 0
-        err = nn_strerror(nn_errno())
-        error("nn_bind: ", err)
-    end
-end
+nn_connect(sock, addr) = ccall((:nn_connect, "libnanomsg"), Int32, (Int32, Ptr{UInt8}), sock, addr)
 
 function nn_recv(s ; blocking=true)
     # define NN_MSG ((size_t) -1)
@@ -70,10 +62,6 @@ function nn_recv(s ; blocking=true)
         Int32,
         (Cint, Ptr{Cchar}, Csize_t, Cint),
         s, pointer(buf), typemax(Csize_t), flags)
-    if bytes < 0
-        err = nn_strerror(nn_errno())
-        error("nn_recv: ", err)
-    end
     msg = unsafe_string(buf[1])
     ccall((:nn_freemsg, "libnanomsg"), Cint, (Ptr{Cchar},), buf[1])
     return msg
@@ -83,46 +71,40 @@ function nn_send(sock, msg; blocking=true)
     sz_msg = length(msg)
     flags = blocking ? 0 : 1 # NN_DONTWAIT=1
     bytes = ccall((:nn_send, "libnanomsg"), Int32, (Int32, Ptr{UInt8}, Csize_t, Int32), sock, msg, sz_msg, flags)
-    if bytes < 0
-        err = nn_strerror(nn_errno())
-        error("nn_bind: ", err)
-    end
     return bytes
 end
 
-function nn_shutdown(s, endpoint_id)
-    rv = ccall((:nn_shutdown, "libnanomsg"), Int32, (Int32, Int32), s, endpoint_id)
-    if rv < 0
-        err = nn_strerror(nn_errno())
-        error("nn_shutdown:", err)
-    end
-    return rv
-end
+nn_shutdown(s, endpoint_id) = ccall((:nn_shutdown, "libnanomsg"), Int32, (Int32, Int32), s, endpoint_id)
 
 # Pipeline example
-
-# TODO having issues with nn_recv
 
 const NN_PROTO_PIPELINE = 5
 const NN_PUSH = NN_PROTO_PIPELINE * 16 + 0
 const NN_PULL = NN_PROTO_PIPELINE * 16 + 1
 
-function node0()
-    url = "ipc:///tmp/pipeline.ipc"
-    s = nn_socket(NN_PULL)
-    println(s)
+function node0(url)
+    sock = nn_socket(NN_PULL)
+    check_err("nn_socket", sock)
     endpoint = nn_bind(s, url)
+    check_err("nn_bind", endpoint)
     while true
         msg = nn_recv(s)
+        check_err("nn_recv", msg)
         println("NODE0: RECEIVED ", msg)
     end
 end
 
 function node1(url, msg)
     sock = nn_socket(NN_PUSH)
-    nn_connect(sock, url)
-    nn_send(sock, msg)
+    check_err("nn_socket", sock)
+    rv = nn_connect(sock, url)
+    check_err("nn_connect", rv)
+    bytes = nn_send(sock, msg)
+    check_err("nn_send", bytes)
+    sleep(1) #  wait for messages to flush before shutting down
+    rv = nn_shutdown(s, 0)
+    check_err("nn_shutdown", rv)
 end
 
-node0()
+node0("ipc:///tmp/pipeline.ipc")
 node1("ipc:///tmp/pipeline.ipc", "hello, world its me")
